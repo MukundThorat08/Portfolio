4b2677ae224a7a999c902b600bf3477309cae94f
MukundThorat <mukundthorat08@gmail.com>
2024-02-09 19:09:14 +0530

    initial

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-59.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"6b2c230b","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"thoratmukund607@gmail.com","afterSubmitCcEmail":"thoratmukund607@gmail.com","afterSubmitBccEmail":"thoratmukund607@gmail.com","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-6b2c230b', 59 );
+});
+ });
